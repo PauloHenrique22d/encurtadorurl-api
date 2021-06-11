764091dc105feab996145d1a282ca4ef7b1480a2
phdsousa <phdsousa>
2021-06-11 01:00:44 -0300

    Criação da primeira etapa do encurtador

--- src/main/java/br/com/encurtadorurl/encurtadorurlapi/EncurtadorurlApiApplication.java
+++ src/main/java/br/com/encurtadorurl/encurtadorurlapi/EncurtadorurlApiApplication.java
@@ -1,10 +1,12 @@
 package br.com.encurtadorurl.encurtadorurlapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class EncurtadorurlApiApplication {
 
